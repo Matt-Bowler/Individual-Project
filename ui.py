a1afccfd7127536f2972c74f64ba6bed04e32cf6
BowlerM <mattbow55@googlemail.com>
2025-02-20 15:56:55 +0000

    basic main menu working

--- ui.py
+++ ui.py
@@ -28,13 +28,13 @@ def render_main_menu():
 
 
     human_vs_human_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((WIDTH // 2 - 100, menu_rect[1] + 2 * PADDING + select_text.get_height(), 200, 50)), 
-                                                            text='Human vs Human', 
+                                                            text="Human vs Human", 
                                                             manager=manager)
     ai_vs_human_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((WIDTH // 2 - 100, menu_rect[1] + 3 * PADDING + select_text.get_height() + human_vs_human_button.rect.height, 200, 50)), 
-                                                        text='AI vs Human', 
+                                                        text="AI vs Human", 
                                                         manager=manager)
     ai_vs_ai_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((WIDTH // 2 - 100, menu_rect[1] + 4 * PADDING + select_text.get_height() + human_vs_human_button.rect.height + ai_vs_human_button.rect.height, 200, 50)), 
-                                                    text='AI vs AI', 
+                                                    text="AI vs AI", 
                                                     manager=manager) 
 
     run = True
@@ -73,3 +73,4 @@ def render_main_menu():
         manager.draw_ui(WIN)
         pygame.display.update()
     return white_is_ai, black_is_ai
+    
